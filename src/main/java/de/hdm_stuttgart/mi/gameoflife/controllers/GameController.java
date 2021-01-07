7afcfaa3f8b4b86e299b703958b3a4a295b1b6e1
Jo Laptop <niedworok@lightword.de>
2021-01-07 21:44:04 +0100

    Add zoomable scroll pane

--- src/main/java/de/hdm_stuttgart/mi/gameoflife/controllers/GameController.java
+++ src/main/java/de/hdm_stuttgart/mi/gameoflife/controllers/GameController.java
@@ -1,10 +1,11 @@
 package de.hdm_stuttgart.mi.gameoflife.controllers;
 
 import de.hdm_stuttgart.mi.gameoflife.controllers.components.UICell;
-import de.hdm_stuttgart.mi.gameoflife.controllers.router.Router;
+import de.hdm_stuttgart.mi.gameoflife.controllers.components.UIZoomableScrollPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,13 +20,27 @@ public class GameController extends PageBaseController {
     @FXML
     private GridPane grid;
 
+    @FXML
+    private HBox wrapper;
+
+    @FXML
+    private UIZoomableScrollPane scrollPane;
+
     public void initialize() {
         int size = 200;
+
+        grid = new GridPane();
+        grid.setHgap(1);
+        grid.setVgap(1);
+
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
                 grid.add(new UICell(), i, j);
             }
         }
+
+        scrollPane = new UIZoomableScrollPane(grid);
+        wrapper.getChildren().add(scrollPane);
     }
 
 }
