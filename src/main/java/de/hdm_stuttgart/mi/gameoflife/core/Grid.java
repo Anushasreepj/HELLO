0e91e12432618dcd24f8f9f222b615e0daee3839
Dominik <dominik@mezler.de>
2021-01-08 21:45:45 +0100

    Fixed Grid Class, Add Javadoc for Grid

--- src/main/java/de/hdm_stuttgart/mi/gameoflife/core/Grid.java
+++ src/main/java/de/hdm_stuttgart/mi/gameoflife/core/Grid.java
@@ -4,27 +4,43 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 
+/**
+ * Basic representation of the grid.
+ * Doesn't know generations but is data structure to be used with a generational system.
+ */
 public class Grid implements IGrid{
 
-    HashSet<Cell> aliveCells;
+    HashSet<Cell> aliveCells = new HashSet<Cell>();
 
+    /**
+     * Sets the state of a cell to dead or alive.
+     * @param cell The cell to be altered
+     * @param isAlive The new state of the cell
+     */
     @Override
-    public void setState(Cell cell, boolean isAlive) {
+    public synchronized void setState(Cell cell, boolean isAlive) {
         if(isAlive) {
-            //Only change if the cell isn't already marked as alive
-            if(!aliveCells.contains(cell)) {
-
-            }
+            //Since using a HashSet, duplicates will be ignored so an extra contains call is not necessary.
+            aliveCells.add(cell);
         } else {
             aliveCells.remove(cell);
         }
     }
 
+    /**
+     * Gets the state of a cell.
+     * @param cell The cell to be informed about
+     * @return The state of the cell
+     */
     @Override
     public boolean getState(Cell cell) {
         return aliveCells.contains(cell);
     }
 
+    /**
+     *
+     * @return A full list of all alive cells
+     */
     @Override
     public Cell[] getAliveCells() {
         return aliveCells.toArray(new Cell[0]);
