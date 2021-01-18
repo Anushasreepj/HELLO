7d0d0cae583a7607c01caa4bdd221b2ac7462ba8
Dominik <dominik@mezler.de>
2021-01-18 17:16:23 +0100

    Add StreamEngine

---
+++ src/main/java/de/hdm_stuttgart/mi/gameoflife/core/engine/StreamEngine.java
@@ -0,0 +1,127 @@
+package de.hdm_stuttgart.mi.gameoflife.core.engine;
+import de.hdm_stuttgart.mi.gameoflife.core.Cell;
+import de.hdm_stuttgart.mi.gameoflife.core.Grid;
+import de.hdm_stuttgart.mi.gameoflife.core.IEngine;
+import de.hdm_stuttgart.mi.gameoflife.core.IGrid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamEngine implements IEngine {
+    private static final Logger logger = LogManager.getLogger(StreamEngine.class);
+
+    private IGrid gameGrid;
+    private Timer timer = new Timer();
+
+    private static final int msPerTick = 50; //Placeholder for reaching 20tps. TODO: Use Settings Object/Config to Control.
+
+    private TimerTask timerTask = new TimerTask() {
+        public void run() { nextGeneration(); }
+    };
+
+    /**
+     * Generates a Engine with a prefilled grid
+     * @param grid The grid to use
+     */
+    public StreamEngine(IGrid grid){
+        gameGrid = grid;
+    }
+
+    /**
+     * Generates a Engine with a new clean grid
+     */
+    public StreamEngine(){
+        gameGrid = new Grid();
+    }
+
+    public void startCalculation() {
+        startInterval(msPerTick);
+        logger.trace("Started automated Calculation");
+    }
+
+
+    public void stopCalculation() {
+        stopInterval();
+        logger.trace("Stopped automated Calculation");
+    }
+
+
+    public void nextGeneration() {
+        synchronized (gameGrid){
+
+            logger.trace("Started calculating next generation");
+
+            HashMap<Cell,Integer> deadCellsToCheck = new HashMap<Cell,Integer>();
+
+            Arrays.stream(gameGrid.getAliveCells()).forEach(cell -> {
+                cell.getNeighbours().stream()
+                        .filter(neighbourCell -> !gameGrid.getState(neighbourCell)) //Only get dead cells
+                        .forEach(neighbourCell -> {
+                            if(deadCellsToCheck.containsKey(neighbourCell))
+                                deadCellsToCheck.put(neighbourCell, deadCellsToCheck.get(neighbourCell)+1); //Add one to neighbour count
+                            else
+                                deadCellsToCheck.put(neighbourCell, 1); //Introduce neighbour count as 1
+                });
+            });
+
+
+            //Calculate next generation for alive cells
+            List<FutureCellState> futureCellStates = Arrays.stream(gameGrid.getAliveCells())
+                    .parallel()
+                    .map(cell ->
+            {
+                FutureCellState futureCellState = new FutureCellState(cell,true);
+                List<Cell> neighbours = futureCellState.getCell().getNeighbours();
+
+                byte aliveNeighbourCount = (byte) cell.getNeighbours().stream().filter(neighbourCell -> gameGrid.getState(neighbourCell)).count();
+
+                if(aliveNeighbourCount<2 | aliveNeighbourCount>3) futureCellState.setAlive(false);
+                return futureCellState;
+
+            }
+            ).collect(Collectors.toList());
+
+            deadCellsToCheck.entrySet().stream().filter(cellByteEntry -> cellByteEntry.getValue()==3).forEach(cellByteEntry -> futureCellStates.add(
+                    new FutureCellState(cellByteEntry.getKey(), true, true)
+            ));
+
+            futureCellStates.stream().filter(futureCellState -> futureCellState.isChanged()).forEach(futureCellState -> {
+                gameGrid.setState(futureCellState.getCell(), futureCellState.isAlive());
+            });
+
+            logger.trace("Finished calculating next generation");
+        }
+    }
+
+
+    public void loadGrid(IGrid grid) {
+        stopCalculation();
+
+        synchronized(gameGrid){
+            gameGrid = grid;
+        }
+        logger.trace("Replaced game grid");
+    }
+
+
+    /**
+     * Starts or Restarts the internal Interval
+     * @param speed
+     */
+    private void startInterval(long speed){
+        timer.cancel();
+        timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask,0, speed);
+    }
+
+    /**
+     * Stops the internal Interval
+     */
+    private void stopInterval(){
+        timer.cancel();
+    }
+
+
+}
