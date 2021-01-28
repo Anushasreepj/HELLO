a1e7859d4efbb1471f66f938f04aae86ef04f3f7
Dominik <dominik@mezler.de>
2021-01-28 12:32:25 +0100

    Add Javadocs

--- src/main/java/de/hdm_stuttgart/mi/gameoflife/core/SimulationSettings.java
+++ src/main/java/de/hdm_stuttgart/mi/gameoflife/core/SimulationSettings.java
@@ -2,12 +2,17 @@ package de.hdm_stuttgart.mi.gameoflife.core;
 
 public class SimulationSettings {
 
-    //Basic optimization Setting
+    //Basic Setting targeted towards optimization
     //Settings for the area the engine should provide updates for, since anything not rendered doesn't need to get processed by the UI.
     private Cell bottomLeftViewCorner = new Cell(-100, -100);
     private Cell topRightViewCorner = new Cell(100, 100);
 
-    public void setViewCorners(Cell corner1, Cell corner2){
+    /**
+     * Sets the corners for the area the engine shall provide updates for. The corners can be any two non-adjacent corners.
+     * @param corner1 First Corner
+     * @param corner2 Second Corner (Non-Adjacent to corner1)
+     */
+    public void setViewCorners(Cell corner1, Cell corner2){ //Ensures right corners even if top left and botton right where given.
         int x1 = corner1.getX();
         int x2 = corner2.getX();
         int y1 = corner1.getY();
@@ -17,10 +22,18 @@ public class SimulationSettings {
         topRightViewCorner = new Cell(Math.max(x1,x2), Math.max(y1,y2));
     }
 
+    /**
+     *
+     * @return The Bottom Left Corner of the area the engine shall provide updates for
+     */
     public Cell getBottomLeftViewCorner(){
         return bottomLeftViewCorner;
     }
 
+    /**
+     *
+     * @return The Top Right Corner of the area the engine shall provide updates for
+     */
     public Cell getTopRightViewCorner(){
         return topRightViewCorner;
     }
@@ -40,10 +53,18 @@ public class SimulationSettings {
     //Tells the engine whether it should calculate synchronized or in parallel.
     private boolean parallelCalculations = false;
 
+    /**
+     * Sets whether the Engine should calculate in parallel or synchronized
+     * @param parallelCalculations
+     */
     public void setParallelCalculations(boolean parallelCalculations){
         this.parallelCalculations = parallelCalculations;
     }
 
+    /**
+     *
+     * @return Should the Engine calculate in parallel or synchronized?
+     */
     public boolean getParallelCalculations(){
         return parallelCalculations;
     }
